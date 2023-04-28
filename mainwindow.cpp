7665802a310011c5dc11e6d042de876128ffffbb
meteistar <gargamel_1987@hotmail.com>
2023-04-28 16:59:39 +0300

    Added cpuwidget implemantation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "sysinfo.h"
+
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , mCpuWidget(this)
 {
     ui->setupUi(this);
+    SysInfo::instance().init();
+    QVBoxLayout* vb = new QVBoxLayout(this);
+    vb->addWidget(&mCpuWidget);
+    ui->centralwidget->setLayout(vb);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include "cpuwidget.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -17,5 +19,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    CpuWidget mCpuWidget;
 };
 #endif // MAINWINDOW_H
